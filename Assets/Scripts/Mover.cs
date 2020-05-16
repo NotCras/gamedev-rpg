d82f00f631ec9cd1baa5129787256409fa1b8d75
NotCras <morrowjo@oregonstate.edu>
2020-05-16 14:22:59 -0700

    basic player movement with navmesh.

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+
+    private NavMeshAgent _navMeshAgent;
+    
+    void Start()
+    {
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        _navMeshAgent.SetDestination(target.position);
+    }
+}
