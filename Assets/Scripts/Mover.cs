205973f479744968f57e4de24be68307888d6ac8
NotCras <morrowjo@oregonstate.edu>
2020-05-18 21:28:30 -0700

    Implemented click to move.

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -5,10 +5,8 @@ using UnityEngine.AI;
 
 public class Mover : MonoBehaviour
 {
-    [SerializeField] private Transform target;
-
     private NavMeshAgent _navMeshAgent;
-    
+
     void Start()
     {
         _navMeshAgent = GetComponent<NavMeshAgent>();
@@ -16,6 +14,21 @@ public class Mover : MonoBehaviour
 
     void Update()
     {
-        _navMeshAgent.SetDestination(target.position);
+        if (Input.GetMouseButtonDown(1))
+        {
+            MoveToCursor();
+        }
+    }
+
+    private void MoveToCursor()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hitInfo;
+        bool hasHit = Physics.Raycast(ray, out hitInfo);
+
+        if (hasHit)
+        {
+            _navMeshAgent.destination = hitInfo.point;
+        }
     }
 }
