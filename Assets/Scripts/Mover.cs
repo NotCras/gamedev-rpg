28d784054ead1ed09e23576262671bd464ff7cd1
NotCras <morrowjo@oregonstate.edu>
2020-05-18 22:30:53 -0700

    implemented character animations with a blendtree.

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
@@ -6,20 +7,24 @@ using UnityEngine.AI;
 public class Mover : MonoBehaviour
 {
     private NavMeshAgent _navMeshAgent;
+    private Animator _animator;
+    
 
     void Start()
     {
         _navMeshAgent = GetComponent<NavMeshAgent>();
+        _animator = GetComponent<Animator>();
     }
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(1))
+        if (Input.GetMouseButton(1))
         {
             MoveToCursor();
         }
+        UpdateAnimator();
     }
-
+    
     private void MoveToCursor()
     {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -30,5 +35,16 @@ public class Mover : MonoBehaviour
         {
             _navMeshAgent.destination = hitInfo.point;
         }
+    }
+    
+    private void UpdateAnimator()
+    {
+        Vector3 velocity = _navMeshAgent.velocity;
+        Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+        float speed = Math.Abs(localVelocity.z); //localVelocity.z;
+        
+        print(speed);
+        
+        _animator.SetFloat("forwardSpeed", speed);
     }
 }
