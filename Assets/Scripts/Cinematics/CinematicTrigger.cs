d55ac3aad7c4e3c8c235f290b022c55a8b790387
NotCras <morrowjo@oregonstate.edu>
2020-06-09 18:58:45 -0700

    added triggerable cinematics. Cinematics are poop.

---
+++ Assets/Scripts/Cinematics/CinematicTrigger.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+namespace RPG.Cinematics
+{
+    public class CinematicTrigger : MonoBehaviour
+    {
+        private bool alreadyTriggered = false;
+        
+        private void OnTriggerEnter(Collider other)
+        {
+
+            if (!alreadyTriggered && other.gameObject.tag == "Player")
+            {
+                GetComponent<PlayableDirector>().Play();
+                alreadyTriggered = true;
+            }
+
+        }
+    }
+}
